81c26f5a35201c738dc715c768922f3217853c0a
clever <ogawa.satoshi@pepabo.com>
2017-04-24 10:19:59 +0900

    Add other parameters for check_plugins
    To set up some parameters for check_plugins in configure file needs to configure some line settigns

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -43,13 +43,13 @@ describe 'mackerel_agent::config' do
       { :check_plugins => { 'access_log' => '/usr/local/bin/check-log --file /var/log/access.log --pattern FATAL' } }
     end
     it { should contain_file('mackerel-agent.conf').with_ensure('present').with_content(%r{^\[plugin.checks.*\]\ncommand = \".*\"$}) }
-    end
+  end
 
   context 'with check_plugins on some parameters' do
     let(:params) do
       { :check_plugins => { 'ssh' => { 'command' => 'ruby /path/to/check-ssh.rb', 'notification_interval' => '6', 'max_check_attempts' => '3', 'check_interval' => '5'} } }
     end
     it { should contain_file('mackerel-agent.conf').with_ensure('present').with_content(%r{^\[plugin.checks.*\]\ncommand = \".*\"\nnotification_interval = \".*\"\nmax_check_attempts = \".*\"\ncheck_interval = \".*\"$}) }
-    end
+  end
 
 end
